a98e1ccbf679c99517de7c776861511ae3d170bf
lauri <lauri.paloheimo@live.com>
2025-04-03 07:07:01 +0300

    Comment

--- Module1-Interactive-Programs/Teht10/Teht10.js
+++ Module1-Interactive-Programs/Teht10/Teht10.js
@@ -1,3 +1,4 @@
+// Not the best solution because with extremely large amounts of dice the list eats a lot of memory potentially even crashing
 'use strict';
 const diceNum = parseInt(prompt('Give the amount of dice'));
 const diceEyeSum = parseInt(prompt('Give me the dice eye sum'));
