25683fd6dbb6d70ba4f3744d9d653c0d6dfab675
lauri <lauri.paloheimo@live.com>
2025-05-08 10:39:38 +0300

    1st commit

--- Module4-APIT-AJAX/T2/T2.js
+++ Module4-APIT-AJAX/T2/T2.js
@@ -9,6 +9,12 @@ searchForm.addEventListener('submit', async function(evt) {
     const response = await fetch(
       `https://api.tvmaze.com/search/shows?q=${searchParam}`);
     const json = await response.json();
+
+    // this one logs all the search results to the console
+    for (let a of json) {
+      console.log(a);
+    }
+    // This one logs just the names of the shows from the search results not sure what the assignment actually wanted so here's both
     for(let a of json) {
       console.log("Name of show :" + a['show']['name'])}
   } catch (error) {
